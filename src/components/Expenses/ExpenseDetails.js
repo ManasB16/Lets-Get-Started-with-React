7c1e928bb528e92b0e1e2d9389b3e2ae8ee2edd0
Manas Bhola <manasbhola@gmail.com>
2024-06-02 19:22:11 +0530

    Listening for Events

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,10 +1,17 @@
 const ExpenseDetails = (props) => {
+  function handleChange(e) {
+    console.log("Button Clicked");
+  }
+
   return (
-    <div className="expense-item__description">
-      <h2>{props.title}</h2>
-      <h2>{props.location}</h2>
-      <div className="expense-item__price">${props.amount}</div>
-    </div>
+    <>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <h2>{props.location}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={handleChange}>Change Title</button>
+    </>
   );
 };
 
